1faee47b76a420f97f372b73903ff2c48ddffa86
dmitrtynovik <cryinstone@gmail.com>
2022-08-25 12:13:03 +1000

    fix:

--- examples/kind_install_operator_with_overrides.sh
+++ examples/kind_install_operator_with_overrides.sh
@@ -5,9 +5,12 @@ override_file_name="operator-values-override.yaml"
 namespace="tanzu-postgres"
 operator_name="postgres-operator"
 offline_path="/home/dmitry/Downloads"
+operator_version="1.8.0"
+filename="postgres-for-kubernetes-v$operator_version"
 
 cwd=$(pwd)
 cd $offline_path
+cd $filename
 
 helm install $operator_name "./operator" \
     --values="./out/$override_file_name" \
